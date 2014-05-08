8ff188a94e64b87bd4ca19820458eb6d3c39c754
Mao Rui <mr.fanyange@gmail.com>
2014-05-08 13:25:05 +0800

    Align equal sign

--- timestring/lib/timestring.rb
+++ timestring/lib/timestring.rb
@@ -3,9 +3,9 @@ require 'active_support/inflector'
 class TimeString
   def initialize(str)
     if m = str.match(/\A([0-1][0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9](\.\d{2})?)/)
-      @hour = m[1].to_i
-      @min = m[2].to_i
-      @sec = m[3].to_f
+      @hour   = m[1].to_i
+      @min    = m[2].to_i
+      @sec    = m[3].to_f
     else
       raise SyntaxError, "Invalid duration time format, need hh:mm:ss(.nn)"
     end
