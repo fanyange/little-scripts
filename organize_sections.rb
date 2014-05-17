bc6984ac518d2eb5f14516e86f543eae71021c26
Mao Rui <mr.fanyange@gmail.com>
2014-05-17 23:25:25 +0800

    Initial cod for organizing sections in my diaries

---
+++ organize_sections.rb
@@ -0,0 +1,28 @@
+class Article
+  def initialize(title)
+    @title = title
+    @sections = 
+    @order = 
+  end
+  
+
+end
+
+class Section
+  def initialize(section_name)
+    @section_name = section_name
+    @ctime
+    @atime
+  end
+end
+
+class Source < File
+
+  def organize(section_name)
+    ctime = 
+    mtime = 
+
+    Section.new(section_name, , )
+  end
+  
+end
